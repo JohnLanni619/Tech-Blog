9abfff508ddd284f5d308ecbcf03fd4bb74261d1
John Lanni <82123623+JohnLanni619@users.noreply.github.com>
2021-07-25 21:38:08 -0400

    added edit post functionality

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -48,6 +48,30 @@ router.post('/', (req, res) => {
     });
 });
 
+router.put('/:id', withAuth, (req, res) => {
+  Post.update(
+    {
+      title: req.body.title
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 router.delete('/:id', withAuth, (req, res) => {
   console.log('id', req.params.id);
   Post.destroy({
